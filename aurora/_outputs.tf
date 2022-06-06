5479c29af2c39eea7f4dfbae58520b24a5855da9
Abe Peters <abe.peters@daugherty.com>
2022-06-06 14:36:06 -0500

    Updated presentation doc

--- aurora/_outputs.tf
+++ aurora/_outputs.tf
@@ -3,3 +3,7 @@ output arn { value = aws_rds_cluster.aurora_cluster.arn}
 output database_name { value = aws_rds_cluster.aurora_cluster.database_name}
 output engine { value = aws_rds_cluster.aurora_cluster.engine}
 output cluster_identifier { value = local.cluster_identifier}
+
+
+
+
